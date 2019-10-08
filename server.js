1b0deb4ff7e56b15e41832addb205e36b0dcfbc5
randomslap <kentokazaki@gmail.com>
2019-10-08 16:58:00 -0700

    history

--- server.js
+++ server.js
@@ -22,9 +22,17 @@ mongoose
 	.catch(err => console.log("err " + err));
 // Send every request to the React app
 // Define any API routes before this runs
-app.get("*", function(req, res) {
-	res.sendFile(path.join(__dirname, "./client/public/index.html"));
-});
+if (process.env.NODE_ENV === "production") {
+	app.use(express.static("client/build"));
+	app.get("/*", function(req, res) {
+		res.sendFile(path.join(__dirname, "./client/build/index.html"));
+	});
+} else {
+	app.use(express.static(path.join(__dirname, "/client/public")));
+	app.get("/*", function(req, res) {
+		res.sendFile(path.join(__dirname, "./client/public/index.html"));
+	});
+}
 
 app.listen(PORT, function() {
 	console.log(`🌎 ==> API server now on port ${PORT}!`);
