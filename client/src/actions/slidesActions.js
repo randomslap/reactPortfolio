5d5d3a86f7ca710bf391976b7003484cf7966436
randomslap <kentokazaki@gmail.com>
2019-10-08 19:42:02 -0700

    history finished

--- client/src/actions/slidesActions.js
+++ client/src/actions/slidesActions.js
@@ -1,21 +1,18 @@
-import history from "../history";
+
 
 export const setFirstSlide = () => {
-	history.push("/about");
 	return {
 		type: "FIRST"
 	};
 };
 
 export const setSecondSlide = () => {
-	history.push("/portfolio");
 	return {
 		type: "SECOND"
 	};
 };
 
 export const setThirdSlide = () => {
-	history.push("/contact");
 	return {
 		type: "THIRD"
 	};
